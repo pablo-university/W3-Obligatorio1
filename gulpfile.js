4eeee5e400cca34f2c86da5eb074fc6b5803e7b7
pablo <pablodelfantexp@gmail.com>
2021-05-15 19:10:55 -0300

    add the basic recourses for the proyect

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+//var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+const { series, src, dest, watch, gulp } = require('gulp');
+const sass = require('sass');
+const fs = require('fs');
+
+function serverBrowserSync(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    // cuando estos archivos cambien recarga
+    watch(["./*.html", "./**/*.scss", "./**/*.js"]).on('change', browserSync.reload);
+}
+
+
+// compilo mi sass
+function scssCompiler(cb) {
+    console.log('>>>tocaste el sass');
+    // uso la api de sass o dart sass
+    const result = sass.renderSync({
+        file: "./assets/scss/index.scss",
+        sourceMap: true,
+        outFile: "out/"
+    })
+    // escribo el archivo con node nativo
+    fs.writeFile('./assets/css/index.css', result.css.toString(), function (err) {
+        if (err) throw err;
+        console.log('>>>Sass guardado!');
+    });
+    cb();
+}
+
+
+// con gulp sass puedo ejecutar esta tarea ahora publica
+exports.sass = series(scssCompiler);
+
+// exporto funcion default
+exports.default = function (cb) {
+    // recarga de servidor
+    serverBrowserSync();
+    // cuando el sass cambie compliamelo
+    watch("./**/*.scss", scssCompiler);
+};
+
+
+// https://www.youtube.com/watch?v=y9LlnLTH87U
