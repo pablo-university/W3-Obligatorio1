937329f46ca3940fdcb28c2685ee79e1a9bbb8e8
pablo <pablodelfantexp@gmail.com>
2021-06-02 08:50:49 -0300

    modify import in index.js, problem with burguerMenu?

--- src/assets/js/hamburger.js
+++ src/assets/js/hamburger.js
@@ -1,5 +1,8 @@
-// new burguer menu
-function burguerMenu() {
-    this.classList.toggle("is-active");
+export default function startBurguerMenu() {
+    // new burguer menu
+    function burguerMenu() {
+        this.classList.toggle("is-active");
+    }
+
+    document.querySelector('.hamburger').addEventListener('click', burguerMenu);
 }
-document.querySelector('.hamburger').addEventListener('click', burguerMenu);
