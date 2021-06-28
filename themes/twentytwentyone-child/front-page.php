8c195998dd729642b5ad4033d469c20344bb5781
pablo <pablodelfantexp@gmail.com>
2021-06-28 09:39:19 -0300

    before modify dynamic menu footer

--- themes/twentytwentyone-child/front-page.php
+++ themes/twentytwentyone-child/front-page.php
@@ -1,1 +1,11 @@
-sdcsdcdsc
+<!-- loop for layout of front-page.php (no used because elementor la caga jaja) -->
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+?>
